acfa0441430a264744774b1fd0668025b41ce84f
trendpeek <habbas33@gmail.com>
2022-10-11 18:19:37 +0500

    api

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://170.187.225.159:3000/",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+
 })
